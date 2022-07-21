e49c7ca0ca95cccaf1ec08f8d0b48d1c4b7864db
Sergey A. Dudkin <sdudkin@inno.tech>
2022-07-21 20:48:26 +0400

    add zabbix file

---
+++ linux/zabbix.sh
@@ -0,0 +1,14 @@
+
+
+
+#https://galaxy.ansible.com/community/zabbix
+
+---
+- name: Using Zabbix collection
+  hosts: localhost
+  collections:
+    - community.zabbix
+
+  roles:
+    - role: zabbix_agent
+      zabbix_agent_server: zabbix.example.com
